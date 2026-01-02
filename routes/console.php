01431600aa9489f63e7c60e904644fd213bdbd03
David Lun <mysticcode@gmail.com>
2026-01-02 02:46:12 +0200

    Adds scheduler

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Facades\Awtrix;
+use App\Facades\Meteo;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::call(function () {
+    Awtrix::meteo(Meteo::vilnius());
+})->everyMinute();
--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [SayController::class, 'index'])->name('say.index');
 Route::post('/say', [SayController::class, 'submit'])->name('say.submit');
 
-Route::get('/meteo', [MeteoController::class, 'index'])->name('meteo');
-
 Route::middleware(IsLocal::class)
     ->group(function () {
         Route::get('/settings', [SettingsController::class, 'index'])->name('settings.index');
 
         Route::post('/settings', [SettingsController::class, 'store'])->name('settings.store');
+
+        Route::get('/meteo', [MeteoController::class, 'index'])->name('meteo');
     });
